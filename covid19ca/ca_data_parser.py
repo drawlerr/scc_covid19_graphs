3b3edd2a54890a889fd590059fc1fd8b28a0948f
calchemyst <anna.r.fuller@gmail.com>
2020-04-04 17:08:52 -0700

    Add Flask server

--- covid19ca/ca_data_parser.py
+++ covid19ca/ca_data_parser.py
@@ -1,56 +1,113 @@
 import csv
 import numpy as np
 import math
+import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib.dates as mdates
 import urllib.request
 
 field_names = ['date','county','state','fips','cases','deaths']
-covid_info = []
 counties = set()
 
 url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
 urllib.request.urlretrieve(url, 'us-counties.csv')
 
 
-with open('us-counties.csv', 'r') as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        if(row['state']=="California"):
-            covid_info.append(row)
-            counties.add(row['county'])
+norcal_counties = {'Santa Cruz': 'California', 'Santa Clara': 'California',  'Napa': 'California',
+                   'Alameda': 'California', 'San Mateo': 'California',
+                   'San Francisco': 'California',
+                   'Marin': 'California', 'Solano': 'California','Contra Costa': 'California'}
 
-for county in counties:
-    with open('{}.csv'.format(county), 'w') as f:
-        writer = csv.DictWriter(f, fieldnames = field_names)
-        writer.writeheader()
+socal_counties = {'Ventura' :'California',  'San Diego':'California', 'Kern':'California',
+                  'Los Angeles':'California', 'San Bernardino' :'California', 'Riverside':'California',
+                  'Orange':'California', 'Kings':'California'}
+
+md_va_dc_counties = {'Carroll': 'Maryland', 'Frederick': 'Maryland','District of Columbia':'District of Columbia',  "Prince George's": 'Maryland', 'Arlington': 'Virginia', 'Fairfax': 'Virginia'}
+
+
+
+
+def get_county_data_from_csv(counties):
+    county_info =[]
+    with open('us-counties.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            for county, state in counties.items():
+                if row['state']== state and row['county'] == county:
+                    county_info.append(row)
+    return county_info
+
+
+def get_date_range(county_info):
+    date_set = set()
+    for row in county_info:
+        date_set.add(row['date'])
+    return date_set
+
+def create_count_csv(counties, covid_info, date_set):
+    for county in counties:
+        county_info = []
+        dates_added = set()
         for row in covid_info:
             if row['county'] == county:
+                dates_added.add(row['date'])
+                county_info.append(row)
+
+        for date in date_set:
+            if date not in dates_added:
+                row = {
+                    'date': date,
+                    'county': county,
+                    'state': "California",
+                    'fips': '00000', #this can be nonsense for now
+                    'cases': 0,
+                    'deaths': 0
+                }
+                county_info.append(row)
+        county_info = sorted(county_info, key = lambda i: i['date'])
+        with open('{}.csv'.format(county), 'w') as f:
+            writer = csv.DictWriter(f, fieldnames=field_names)
+            writer.writeheader()
+            for row in county_info:
                 writer.writerow(row)
 
-print(counties)
+def plot_counties(counties, filename):
+    plt.switch_backend('Agg')
+    plt.subplots()
 
-for county in counties:
-    data = pd.read_csv('{}.csv'.format(county))
-    date = data['date']
-    cases = data['cases']
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
 
-    plt.plot(date, cases)
-    plt.gcf().autofmt_xdate()
+    for county, state in counties.items():
+        data = pd.read_csv('{}.csv'.format(county))
+        data.sort_values('date')
+        cases = data['cases']
+        date = data['date']
+        plt.gcf().autofmt_xdate()
+        ax.plot(date, cases, label="{},{}".format(county, state))
+
+
+        # rotates and right aligns the x labels, and moves the bottom of the
+        # axes up to make room for them
+
+    plt.grid(True)
+    plt.legend()
+    plt.title("COVID19 Cases")
     plt.ylabel('Cases (log scale)')
     plt.xlabel('Date')
-    plt.title('Covid19 Cases: {} County'.format(county))
-    fig, ax = plt.subplots(1,1)
-    ax.plot(date,cases)
+    plt.xticks(date[::5],rotation=90)
+    plt.yscale('log')
 
-    ax.grid(True)
-    ax.set_title('Covid19 Cases: {} County'.format(county))
-    # rotates and right aligns the x labels, and moves the bottom of the
-    # axes up to make room for them
-    plt.xticks(date[::3],rotation=90)
-    ax.set_yscale('log')
-    plt.savefig('log_{}.png'.format(county))
+    plt.savefig(filename)
 
 
-    plt.close()
+def main():
+    counties = norcal_counties
+    county_info =get_county_data_from_csv(counties)
+    date_range = get_date_range(county_info)
+    create_count_csv(counties.keys(), county_info, date_range)
+    plot_counties(counties)
+
+if __name__=="__main__":
+    main()
---
+++ covid19ca/data_parser_server.py
@@ -0,0 +1,51 @@
+from flask import Flask, abort
+from flask import render_template
+import os
+import logging
+from flask import render_template, flash, redirect, session, url_for, request, g
+from werkzeug.exceptions import BadRequestKeyError
+
+from covid19ca import ca_data_parser
+
+app = Flask(__name__)
+
+STATIC_FOLDER = os.path.join('static')
+#full_filename = os.path.join(STATIC_FOLDER, 'log_graph_CA.png')
+
+county_states_mapping = []
+with open(os.path.join(STATIC_FOLDER, 'county_state_mapping')) as f:
+    for line in f.readlines():
+        county_states_mapping.append(line.strip())
+
+
+@app.route('/get_graph', methods=['GET','POST'])
+def handle_graph():
+    logger = app.logger
+    logger.debug("form: %s", request.form)
+    try:
+        graph = request.form['graph']
+    except BadRequestKeyError:
+        logger.warning("no graph parameter!")
+        abort(400)
+
+    logger.debug("graph parameter: [%s]", graph)
+    splits = graph.split(":")
+    if len(splits) != 2:
+        logger.warning("invalid graph parameter!")
+        abort(400)
+
+    state, county = splits[:2]
+    counties = { county: state}
+    county_info = ca_data_parser.get_county_data_from_csv(counties)
+    date_range = ca_data_parser.get_date_range(county_info)
+    ca_data_parser.create_count_csv(counties.keys(), county_info, date_range)
+
+    filename = state + county + ".png"
+    full_filename = os.path.join(STATIC_FOLDER, filename)
+    ca_data_parser.plot_counties(counties, full_filename)
+    return render_template('graph.html', covid_graph=full_filename, county_states=county_states_mapping)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', county_states=county_states_mapping)
