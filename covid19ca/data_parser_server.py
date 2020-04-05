40d11b3d792875aa5a4a92d65c5aa4ed3bbd62cd
Dennis Lawler <dlawler@gmail.com>
2020-04-05 14:32:26 -0700

    Quick cleanup.

--- covid19ca/data_parser_server.py
+++ covid19ca/data_parser_server.py
@@ -1,9 +1,7 @@
 from flask import Flask, abort
-from flask import render_template
 import json
 import os
-import logging
-from flask import render_template, flash, redirect, session, url_for, request, g
+from flask import render_template, request
 from werkzeug.exceptions import BadRequestKeyError
 
 from covid19ca import ca_data_parser
@@ -11,16 +9,14 @@ from covid19ca import ca_data_parser
 app = Flask(__name__)
 
 STATIC_FOLDER = os.path.join('static')
-#full_filename = os.path.join(STATIC_FOLDER, 'log_graph_CA.png')
 
-state_county_dict = {}
 with open(os.path.join(STATIC_FOLDER, 'county_state_mapping')) as f:
-    state_county_dict= json.load(f)
+    state_county_dict = json.load(f)
 
 print(state_county_dict)
 
 
-@app.route('/get_graph', methods=['GET','POST'])
+@app.route('/get_graph', methods=['GET', 'POST'])
 def handle_graph():
     logger = app.logger
     logger.debug("form: %s", request.form)
@@ -29,15 +25,17 @@ def handle_graph():
     except BadRequestKeyError:
         logger.warning("no graph parameter!")
         abort(400)
+        return
 
     logger.debug("graph parameter: [%s]", graph)
     splits = graph.split(":")
     if len(splits) != 2:
         logger.warning("invalid graph parameter!")
         abort(400)
+        return
 
     state, county = splits[:2]
-    counties = { county: state}
+    counties = {county: state}
     county_info = ca_data_parser.get_county_data_from_csv(counties)
     date_range = ca_data_parser.get_date_range(county_info)
     ca_data_parser.create_count_csv(counties.keys(), county_info, date_range)
@@ -50,7 +48,7 @@ def handle_graph():
 
 @app.route('/')
 def index():
-    return render_template('index.html',  county_states=state_county_dict)
+    return render_template('index.html', county_states=state_county_dict)
 
 
 if __name__ == "__main__":
