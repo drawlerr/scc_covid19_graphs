fb3894879006fdb26d2c44e27421c0b746d93d3a
Dennis Lawler <dlawler@gmail.com>
2020-04-13 12:16:43 -0700

    Update mappings, track unknown fips codes.

--- covid19ca/update_csv_and_mappings.py
+++ covid19ca/update_csv_and_mappings.py
@@ -11,22 +11,24 @@ url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-countie
 urllib.request.urlretrieve(url, 'us-counties.csv')
 
 fips_county_dict = {}
+unknowns = set()
 
 with open('us-counties.csv', 'r') as f:
     reader = csv.DictReader(f)
     for row in reader:
+        place = f"{row['state']} - {row['county']}"
         try:
             fips = int(row['fips'])
         except ValueError:
             if row['county'] == "New York City":
                 fips = 36061
             else:
+                unknowns.add(place)
                 continue
-        state = row['state']
-        county = row['county']
-        name = f"{state} - {county}"
-        fips_county_dict[name] = fips
+        fips_county_dict[place] = fips
 
+unknowns_list = "\n" + "\n".join(sorted(unknowns))
+print(f"{len(unknowns)} counties with missing FIPS IDs: {unknowns_list}")
 fips_county_dict = OrderedDict(sorted(fips_county_dict.items()))
 
 with open(os.path.join(STATIC_FOLDER, 'fips_county_mapping.json'), 'w') as f:
